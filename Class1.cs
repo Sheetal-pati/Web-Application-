dae6a91816b0587742a82c28bb53ddbde44a7e25
SHEETAL PATI <36594056+Sheetal-pati@users.noreply.github.com>
2019-07-16 15:17:50 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public static string changedate(string date)
+    {
+        String result = "", yr = "", mon = "", dt = "", hr = "", min = "", sec = "";
+        yr = date.Substring(0, 4);
+        mon = date.Substring(4, 2);
+        dt = date.Substring(6, 2);
+        hr = date.Substring(8, 2);
+        min = date.Substring(10, 2);
+        sec = date.Substring(12, 2);
+        result = dt + "/" + mon + "/" + yr + "\n" + hr + ":" + min + ":" + sec;
+        return result;
+    }
+}
---
+++ dbcls.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+/// <summary>
+/// Summary description for dbcls
+/// </summary>
+public class dbcls
+{
+	public dbcls()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+    public static SqlConnection CONN()
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.AppSettings["SQLCON"]);
+        if (con.State == System.Data.ConnectionState.Closed)
+        {
+            con.Open();
+        }
+        else
+        {
+            con.Close();
+        }
+        return con;
+    }
+    public static DataTable RETURN_DT(string QUERY)
+    {
+        DataTable dt = new DataTable();
+        SqlDataAdapter ada = new SqlDataAdapter(QUERY,CONN());
+        ada.Fill(dt);
+        return dt;
+
+    }
+
+
+}
