335cf223cc6805fb08179dd3af04e149b0d9f31e
SHEETAL PATI <36594056+Sheetal-pati@users.noreply.github.com>
2019-07-18 11:10:04 +0530

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -27,4 +27,25 @@ public class Class1
         result = dt + "/" + mon + "/" + yr + "\n" + hr + ":" + min + ":" + sec;
         return result;
     }
+	 public static string date(string date)
+    {
+        String Result;
+        String  yr = "", mon = "", dt = "";
+        //Result = date.Substring(0,8);
+        yr = date.Substring(0, 4);
+        mon = date.Substring(4, 2);
+        dt = date.Substring(6, 2);
+        Result = yr + "/" + mon + "/" + dt;
+
+        return Convert.ToDateTime( Result).ToString("yyyy-MM-dd");
+    }
+    public static string time(string date)
+    {
+        String result = "", hr = "", min = "", sec = "";
+         hr = date.Substring(8, 2);
+        min = date.Substring(10, 2);
+        sec = date.Substring(12, 2);
+        result = hr + ":" + min + ":" + sec;
+        return result;
+    }
 }
